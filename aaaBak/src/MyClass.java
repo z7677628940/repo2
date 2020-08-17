9fde7fb8e29971e261da369966152d916f7ddab9
z7677628940 <767762894@qq.com>
2020-08-17 21:04:00 +0800

    将java工程提交到版本库

---
+++ aaaBak/src/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6,7,8,9};
+        for (int i : num) {
+            if (i == 5){
+
+            }
+        }
+    }
+
+}
